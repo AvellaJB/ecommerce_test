9ca5b7adac219ec1b3298725f1eba342a1518b20
AvellaJB <jeanbaptiste.avella@gmail.com>
2020-05-02 17:55:08 +0200

    Creation conflit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $pageTitle = "Mon premier site de E-Commerce";
 
 include("inc/header.php");
 echo "Bonjour monsieur Avella";
-echo "Commit branche";
+echo "Commit conflit";
 
 include("inc/footer.php");
 ?>
