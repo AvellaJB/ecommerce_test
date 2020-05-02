0de664767ef05076f71e93dd57713e6a1d920b52
AvellaJB <jeanbaptiste.avella@gmail.com>
2020-05-02 18:43:00 +0200

    premier commit sur Github

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ include("inc/header.php");
 echo "BonjourAPihdioazhdoiazhdoihzad monsieur Avella";
 echo "Commit conflit";
 echo  "Commit develop2";
+echo "Commit sur master Github";
 
 
 include("inc/footer.php");
